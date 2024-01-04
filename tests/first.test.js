16aa894789482d7a98271289898cc330ec313671
Jorge Hurtado <lhgjorge@gmail.com>
2024-01-04 16:09:45 -0400

    Useless tests deleted

--- tests/first.test.js
+++
@@ -1,7 +0,0 @@
-import {test, expect} from '@playwright/test';
-
-test('Trial test', async ({page}) => {
-
-    await page.goto('https://saucedemo.com/');
-    await expect(page).toHaveTitle('Swag Labs');
-})
