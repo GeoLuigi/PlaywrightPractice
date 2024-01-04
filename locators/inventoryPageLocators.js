a4f744fed2858b873f6ff001e4afd162f0bf5627
Jorge Hurtado <lhgjorge@gmail.com>
2024-01-04 16:55:18 -0400

    Locators fixed

--- locators/inventoryPageLocators.js
+++ locators/inventoryPageLocators.js
@@ -1,5 +1,5 @@
 export const inventoryPageLocators = {
-    onesieClothingBtn: 'add-to-cart-sauce-labs-onesie',
-    fleeceJacketClothingBtn: 'add-to-cart-sauce-labs-fleece-jacket',
-    shoppingCartBtn: 'shopping_cart_container',
+    onesieClothingBtn: '#add-to-cart-sauce-labs-onesie',
+    fleeceJacketClothingBtn: '#add-to-cart-sauce-labs-fleece-jacket',
+    shoppingCartBtn: '#shopping_cart_container',
 }
